6e3b569e2c8bd09a2dc64a07c6f1cd3090e24018
qunwang6 <qunwang6@gmail.com>
2018-08-03 17:12:59 +0800

    beta 3

--- showAlert2/showAlert/AboutViewController.swift
+++ showAlert2/showAlert/AboutViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class AboutViewController: UIViewController {
 
+    @IBOutlet weak var webView: UIWebView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let url = Bundle.main.url(forResource:"BullsEye",withExtension:"html") {
+            if let htmlData = try? Data(contentsOf: url){
+                let baseURL = URL(fileURLWithPath: Bundle.main.bundlePath)
+                webView.load(htmlData,mimeType: "text/html",textEncodingName: "UTF-8",baseURL: baseURL)
+                
+            }
+        }
+    }
     //AboutViewController Class要重新命名
     //check Inheri Module From Target
     @IBAction func close(_ sender: Any) {
