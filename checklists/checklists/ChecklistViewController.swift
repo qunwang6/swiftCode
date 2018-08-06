b579651a5d84c626da650d52f7723ba6ebf096d6
qunwang6 <qunwang6@gmail.com>
2018-08-06 16:43:10 +0800

    checklists 1

--- checklists/checklists/ChecklistViewController.swift
+++ checklists/checklists/ChecklistViewController.swift
@@ -9,6 +9,18 @@
 import UIKit
 
 class ChecklistViewController: UITableViewController{
+    var row0text = "Walk the dog"
+    var row1text = "Brush my teeth"
+    var row2text = "Learn iOS development"
+    var row3text = "Soccer practice"
+    var row4text = "Eat ice cream"
+    
+    var row0checked = false
+    var row1checked = true
+    var row2checked = true
+    var row3checked = false
+    var row4checked = false
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section:Int) -> Int {
         return 10
     }
@@ -16,19 +28,20 @@ class ChecklistViewController: UITableViewController{
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath:IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem",for :indexPath)
         let label = cell.viewWithTag(1000) as! UILabel
-        /*
+        
         if indexPath.row == 0 {
-            label.text = "Walk the dog"
+            label.text = row0text
         } else if indexPath.row == 1 {
-            label.text = "Brush my teeth"
+            label.text = row1text
         } else if indexPath.row == 2 {
-            label.text = "Learn iOS development"
+            label.text = row2text
         } else if indexPath.row == 3 {
-            label.text = "Soccer practice"
+            label.text = row3text
         } else if indexPath.row == 4 {
-            label.text = "Eat ice cream"
+            label.text = row4text
         }
-         */
+ 
+        /*
         if indexPath.row  % 5 == 0 {
             label.text = "Walk the dog"
         } else if indexPath.row % 5 == 1 {
@@ -40,7 +53,120 @@ class ChecklistViewController: UITableViewController{
         } else if indexPath.row % 5 == 4 {
             label.text = "Eat ice cream"
         }
+        */
+        configureCheckmark(for:cell,at:indexPath)
         return cell
+    }
+    /*
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let cell = tableView.cellForRow(at: indexPath) {
+            if indexPath.row == 0 {
+                row0checked = !row0checked
+                if row0checked {
+                    cell.accessoryType = .checkmark
+                } else {
+                    cell.accessoryType = .none
+                }
+            } else if indexPath.row == 1 {
+                row1checked = !row1checked
+                if row1checked {
+                    cell.accessoryType = .checkmark
+                } else {
+                    cell.accessoryType = .none
+                }
+            } else if indexPath.row == 2 {
+                row2checked = !row2checked
+                if row1checked {
+                    cell.accessoryType = .checkmark
+                } else {
+                    cell.accessoryType = .none
+                }
+            } else if indexPath.row == 3 {
+                row3checked = !row3checked
+                if row3checked {
+                    cell.accessoryType = .checkmark
+                } else {
+                    cell.accessoryType = .none
+                }
+            } else if indexPath.row == 4 {
+                row4checked = !row4checked
+                if row4checked {
+                    cell.accessoryType = .checkmark
+                } else {
+                    cell.accessoryType = .none
+                }
+            }
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+    */
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        /*
+        if let cell = tableView.cellForRow(at: indexPath) {
+            var isChecked = false
+            if indexPath.row == 0 {
+                row0checked = !row0checked
+                isChecked = row0checked
+            } else if indexPath.row == 1 {
+                row1checked = !row1checked
+                isChecked = row1checked
+            } else if indexPath.row == 2 {
+                row2checked = !row2checked
+                isChecked = row2checked
+            } else if indexPath.row == 3 {
+                row3checked = !row3checked
+                isChecked = row3checked
+            } else if indexPath.row == 4 {
+                row4checked = !row4checked
+                isChecked = row4checked
+            }
+            
+            if isChecked {
+                cell.accessoryType = .checkmark
+            } else {
+                cell.accessoryType = .none
+            }
+        }
+        */
+        if let cell = tableView.cellForRow(at: indexPath) {
+            if indexPath.row == 0 {
+                row0checked = !row0checked
+            } else if indexPath.row == 1 {
+                row1checked = !row1checked
+            } else if indexPath.row == 2 {
+                row2checked = !row2checked
+            } else if indexPath.row == 3 {
+                row3checked = !row3checked
+            } else if indexPath.row == 4 {
+                row4checked = !row4checked
+            }
+            configureCheckmark(for:cell,at:indexPath)
+            
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+    
+    func configureCheckmark(for cell:UITableViewCell,at indexPath:IndexPath) {
+        var isChecked = false
+        if indexPath.row == 0 {
+            isChecked = row0checked
+        } else if indexPath.row == 1 {
+            isChecked = row1checked
+        } else if indexPath.row == 2 {
+            isChecked = row2checked
+        } else if indexPath.row == 3{
+            isChecked = row3checked
+        } else if indexPath.row == 4 {
+            isChecked = row4checked
+        }
+        if isChecked {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
     }
 }
 
